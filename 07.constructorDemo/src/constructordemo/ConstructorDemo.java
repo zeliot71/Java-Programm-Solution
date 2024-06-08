f77cf1f23f03aaec8b504d3e88cab972f80f6287
AliyenSha <zihadshai701@gmail.com>
2024-06-09 00:33:57 +0600

    constructor

---
+++ 07.constructorDemo/src/constructordemo/ConstructorDemo.java
@@ -0,0 +1,15 @@
+
+package constructordemo;
+public class ConstructorDemo {
+
+    public static void main(String[] args) 
+    {
+        myClass objClass = new myClass(100);
+        myClass objClass2 = new myClass(10.315515);
+        objClass.demo();
+        objClass2.demo();
+        
+        
+    }
+    
+}
---
+++ 07.constructorDemo/src/constructordemo/myClass.java
@@ -0,0 +1,22 @@
+
+package constructordemo;
+public class myClass {
+    
+     myClass(int x)
+     {
+         //int m =x;
+         System.out.println(x);
+
+     }
+     
+        myClass(double x)
+     {
+         //int m =x;
+         System.out.println(x);
+     }
+    
+    public void demo()
+    {
+        System.out.println("This is normal method..");
+    }
+}
