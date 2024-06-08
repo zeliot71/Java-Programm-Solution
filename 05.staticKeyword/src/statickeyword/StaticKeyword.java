400b91bc5777ef4ccfadb08edaef50f53e12c54a
AliyenSha <zihadshai701@gmail.com>
2024-06-09 00:33:24 +0600

    everything about static keywords

---
+++ 05.staticKeyword/src/statickeyword/StaticKeyword.java
@@ -0,0 +1,9 @@
+package statickeyword;
+public class StaticKeyword {
+
+    public static void main(String[] args) {
+       //System.out.println(newClass.a);
+       newClass.display();
+    }
+    
+}
---
+++ 05.staticKeyword/src/statickeyword/newClass.java
@@ -0,0 +1,13 @@
+package statickeyword;
+
+public class newClass {
+
+    public static void display()
+    {
+        int a = 10;
+        int b=12;
+        int c=a+b;
+        System.out.println(c);
+    }
+    
+}
