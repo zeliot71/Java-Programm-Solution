1e632c8cb3445864bb8d03ae7b9a2924c3571d3f
AliyenSha <zihadshai701@gmail.com>
2024-06-09 00:31:47 +0600

    Java (OOP) learning journey started

---
+++ 01.Demo/src/main/java/com/mycompany/demo/Demo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demo;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        myClass objectVariable = new myClass();
+        
+        objectVariable.myFunction();
+        objectVariable.myFunction2();
+    }
+}
---
+++ 01.Demo/src/main/java/com/mycompany/demo/myClass.java
@@ -0,0 +1,12 @@
+package com.mycompany.demo;
+
+public class myClass {
+    public void myFunction(){
+        System.out.println("Welcome to Java Programming..");
+    }
+    
+    public void myFunction2(){
+        System.out.println("We'll learn OOP with Java!");
+    }
+    
+}
