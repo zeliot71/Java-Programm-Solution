b5b3a03b02ee881ddaf26d71874f151326287afa
AliyenSha <zihadshai701@gmail.com>
2024-06-09 00:32:55 +0600

    functions parameter

---
+++ 04.FuntionParameter/src/funtionparameter/FuntionParameter.java
@@ -0,0 +1,14 @@
+package funtionparameter;
+
+
+public class FuntionParameter {
+
+    public static void main(String[] args) {
+        System.out.println("Simple calculuation program..");
+        
+        addClass objectHolder = new addClass();
+        objectHolder.add(20,30);
+        objectHolder.mul(10,20);
+    }
+    
+}
---
+++ 04.FuntionParameter/src/funtionparameter/addClass.java
@@ -0,0 +1,12 @@
+package funtionparameter;
+
+public class addClass {
+    public void add(int a,int b){
+        int c=a+b;
+        System.out.println(c);
+    }
+    public void mul(int a,int b){
+        int c=a*b;
+        System.out.println(c);
+    }
+}
